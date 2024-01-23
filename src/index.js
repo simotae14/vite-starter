e28a402892b9096b1ac06a5da6eaf2cd406c7f76
Simone Taeggi <startae14@gmail.com>
2024-01-23 18:09:27 +0100

    Dynamic Imports example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
+// import { initializeCounter } from './counter.js';
+
 console.log('hello from index.js');
 
-import { initializeCounter } from './counter.js';
-
-initializeCounter(); // initialize the counter
-
-// override the h2 text
-document.querySelector('h2').textContent = 'Hello from index.js';
+import('./counter').then(({ initializeCounter }) => {
+  initializeCounter();
+}); // this one return a Promise
