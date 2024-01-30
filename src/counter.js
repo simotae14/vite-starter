c8daf305447b5a6c0a2b40f20697a2c9e1c06f46
Simone Taeggi <startae14@gmail.com>
2024-01-31 00:25:04 +0100

    Import CSS & CSS Modules: with import of css in js file

--- src/counter.js
+++ src/counter.js
@@ -1,3 +1,7 @@
+import { addBanner } from './banner.js';
+
+import('./style.css');
+
 export const initializeCounter = (doc = globalThis.document) => {
   const countElement = doc.getElementById('count');
   const incrementButton = doc.getElementById('increment');
